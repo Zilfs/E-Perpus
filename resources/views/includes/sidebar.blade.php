0178ea029a4936188fb53b0ad14196df554a83eb
Zilfs <rheinlandcast@gmail.com>
2024-01-30 07:48:23 +0700

    refactor(view): layouting navbar and sidebar

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -57,40 +57,10 @@
                     <span class="nav-link-text ms-1">RTL</span>
                 </a>
             </li>
-            <li class="nav-item mt-3">
-                <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link " href="/template/pages/profile.html">
-                    <div
-                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
-                    </div>
-                    <span class="nav-link-text ms-1">Profile</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link " href="/template/pages/sign-in.html">
-                    <div
-                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="ni ni-single-copy-04 text-warning text-sm opacity-10"></i>
-                    </div>
-                    <span class="nav-link-text ms-1">Sign In</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link " href="/template/pages/sign-up.html">
-                    <div
-                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="ni ni-collection text-info text-sm opacity-10"></i>
-                    </div>
-                    <span class="nav-link-text ms-1">Sign Up</span>
-                </a>
-            </li>
         </ul>
     </div>
-    <div class="sidenav-footer mx-3 ">
-
-        <a class="btn btn-danger btn-sm mb-0 w-100" href="{{ route('logout-pengelola') }}" type="button">Log Out</a>
+    <div class="sidenav-footer mx-3">
+        <a class="btn btn-danger btn-sm mb-0 w-100" href="{{ route('logout-pengelola') }}" type="button">Log
+            Out</a>
     </div>
 </aside>
