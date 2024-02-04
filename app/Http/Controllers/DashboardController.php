9a0acc4e8847188bb88c6a6402b99ca886fb1917
Zilfs <rheinlandcast@gmail.com>
2024-02-04 23:03:24 +0700

    perf(view): table peminjaman hari ini di index dashboard pengelola

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,7 @@ use App\Models\Buku;
 use App\Models\KategoriBuku;
 use App\Models\Peminjaman;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -16,12 +17,14 @@ class DashboardController extends Controller
         $jumlah_buku = Buku::all()->count();
         $kategori_buku = KategoriBuku::all()->count();
         $jumlah_pembaca = User::where('role', 'PEMINJAM')->get()->count();
+        $peminjaman_hari_ini = Peminjaman::with(['buku', 'user'])->where('tanggal_peminjaman', Carbon::today())->orWhere('tanggal_pengembalian', Carbon::today())->get();
 
         return view('pages.pengelola.index', [
             'buku_terpinjam' => $buku_terpinjam,
             'jumlah_buku' => $jumlah_buku,
             'kategori_buku' => $kategori_buku,
             'jumlah_pembaca' => $jumlah_pembaca,
+            'peminjaman_hari_ini' => $peminjaman_hari_ini,
         ]);
     }
 }
