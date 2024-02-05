a33998474f59febd9b02c5bf2c1b9a547b2814c0
Zilfs <rheinlandcast@gmail.com>
2024-02-05 22:34:04 +0700

    refactor: responsive data buku blom dikembalikan

--- resources/views/pages/pengelola/peminjaman/index.blade.php
+++ resources/views/pages/pengelola/peminjaman/index.blade.php
@@ -76,14 +76,23 @@
                             @forelse ($terlambat as $item)
                                 <li
                                     class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                                    <div class="d-flex align-items-center">
-                                        <button
-                                            class="btn btn-icon-only btn-rounded btn-outline-success mb-0 me-3 btn-sm d-flex align-items-center justify-content-center"><i
-                                                class="fas fa-arrow-up"></i></button>
-                                        <div class="d-flex flex-column">
-                                            <h6 class="mb-1 text-dark text-sm">{{ $item->buku->judul }}</h6>
-                                            <span class="text-xs">{{ $item->user->nama_lengkap }}</span>
-                                            <span class="text-xs">{{ $item->user->email }}</span>
+                                    <div class="d-flex align-items-center row">
+                                        <div class="col-12 col-sm-5 d-flex align-items-center">
+                                            <div
+                                                class="btn-rounded btn-outline-success mb-0 me-3 btn-sm d-flex align-items-center justify-content-center">
+                                                <i class="fa-solid fa-circle-exclamation text-warning"></i>
+                                            </div>
+                                            <div class="d-flex flex-column">
+                                                <h6 class="mb-1 text-dark text-sm">{{ $item->buku->judul }}</h6>
+                                                <span class="text-xs">{{ $item->user->nama_lengkap }}</span>
+                                                <span class="text-xs">{{ $item->user->email }}</span>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-12 col-sm-5 mt-2 d-flex flex-column row h-100 ms-3">
+                                            <span class="text-black text-xs mt-0 col-12">Tanggal seharusnya dikembalikan
+                                                :</span>
+                                            <span class="text-xs col-12">{{ $item->tanggal_pengembalian }}</span>
                                         </div>
                                     </div>
                                 </li>
