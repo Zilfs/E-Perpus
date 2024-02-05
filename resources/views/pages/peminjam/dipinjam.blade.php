e4ed1b825f70a8d78ebb5d1c4b0e35e9034766ab
Zilfs <rheinlandcast@gmail.com>
2024-02-05 22:51:46 +0700

    refactor: responsive view in pages.peminjam.dipinjam

--- resources/views/pages/peminjam/dipinjam.blade.php
+++ resources/views/pages/peminjam/dipinjam.blade.php
@@ -17,16 +17,24 @@
                     <div class="card-body pt-4 p-3">
                         <ul class="list-group">
                             @forelse ($data as $item)
-                                <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
-                                    <div class="d-flex flex-column">
+                                <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg row">
+                                    <div class="d-flex flex-column col-12 col-md-6">
                                         <h6 class="mb-3 text-sm">{{ $item->buku->judul }}</h6>
                                         <span class="text-xs">Tanggal Peminjaman : <span
                                                 class="text-dark ms-sm-2 font-weight-bold">{{ $item->tanggal_peminjaman }}</span></span>
                                         <span class="text-xs">Tanggal Pengembalian : <span
                                                 class="text-dark ms-sm-2 font-weight-bold">{{ $item->tanggal_pengembalian }}</span></span>
+                                        @if ($item->tanggal_pengembalian < $today)
+                                            <div class="d-flex align-items-center mt-3"> <i
+                                                    class="fa-solid fa-circle-exclamation text-warning me-2"></i>
+                                                <h6 class="text-sm text-warning mt-2">Buku belum dikembalikan, Sudah lewat
+                                                    batas
+                                                    pengembalian</h6>
+                                            </div>
+                                        @endif
                                     </div>
-                                    <div class="ms-auto text-end">
-                                        <a class="btn btn-link text-danger text-gradient px-3 mb-0"
+                                    <div class="ms-auto text-end col-12 col-md-6">
+                                        <a class="btn btn-danger px-3 mb-0"
                                             href="{{ route('kembalikan-buku', $item->id) }}">Kembalikan buku</a>
                                     </div>
                                 </li>
